9d2ba13baf6d46658d15626e0181603cfd12fc05
clo-or <duzyyeicg09@gmail.com>
2025-12-15 13:47:26 +0900

    Add download button for heart mask image

--- main..py
+++ main..py
@@ -22,3 +22,15 @@ st.set_page_config(
 
 # 타이틀 텍스트 출력
 st.title("C321050 이승아 데이터 시각화 3차 시험")
+
+
+# :blue[위젯]
+
+### : orange [다운로드 버튼: st.download_button()]
+with open("data/heart_mask.png", "rb") as file:
+    st.download_button(
+        label='이미지 파일 다운로드',  # 버튼 라벨
+        data=file,                    # 다운로드할 파일 경로
+        file_name='image.png',        # 다운로드 파일명
+        mime='image/png'              # 파일 형식
+    )
