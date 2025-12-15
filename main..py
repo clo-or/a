8aae511a883dabeda9357208d4ac575e35ef7e14
clo-or <duzyyeicg09@gmail.com>
2025-12-15 15:10:13 +0900

    Update main..py

--- main..py
+++ main..py
@@ -53,9 +53,7 @@ st.text('위에서 언급했던 것과 같이 음식, 전통 등 한국의 라�
 
 if st.button('네트워크 시각화 이미지 원형'):
     st.image("data/naver_news_circle_network.png", use_container_width=True)
-st.text('음식(떡볶이, 김밥, 라면)등이 대체적으로 이어져있고, 라이프스타일, 전통, 호랑이, 관광등이 형성되어있는 것을 보아, 대표적인 한국 라이프스타일과 문화를 볼 수 있는 영화가 팬덤 형성의 핵심 요인이 되었다는 것을 확인할 수 있다.')
-if st.button('네트워크 시각화 이미지'):
-    st.image("data/naver_news_network.png", use_container_width=True)
+
              
 st.header("워드클라우드와 네트워크 시각화는 뉴스의 글로 진행하였다.")
 st.subheader('seaborn 상위 언급 단어이다.')
