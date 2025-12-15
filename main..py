06a92e9a19384cd3ddbc2c4125bfa8ab7a59cfa4
clo-or <duzyyeicg09@gmail.com>
2025-12-15 13:45:05 +0900

    Update main..py

--- main..py
+++ main..py
@@ -1,1 +1,16 @@
+import streamlit as st
+
+st.set_page_config(  # 페이지 설정
+    page_title="케이팝 데몬 헌터스 팬덤 형성 요인 분석",  # 페이지 Tab의 타이틀
+    page_icon="",  # 페이지 Tab의 아이콘
+    layout="wide",  # 페이지 레이아웃: centered, wide
+    # 사이드바 초기 상태 : auto, collapsed, expanded
+    initial_sidebar_state="expanded",
+    # 페이지 오른쪽 상부의 메뉴에 추가할 메뉴 항목: Get help, Report a bug, About
+    menu_items={
+        'Get help': "https://docs.streamlit.io",  # URL만
+        'Report a bug': "https://streamlit.io",  # URL만
+        'About': "## C321050 이승아"
+    }
+)
 
